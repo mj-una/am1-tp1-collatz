939ef1d962c050cdcdddf6f5c2bc9bc13bd132d4
mj-una <133172570+mj-una@users.noreply.github.com>
2023-06-28 02:20:14 -0300

    tp_1

--- sketch.js
+++ sketch.js
@@ -1,114 +1,159 @@
+// collatz (3n + 1)
 // martin julio
-// 19-6-23
+// 27-6-23
 
-let anguloPar = 0.15;
-let anguloImpar = -0.15;
-let longitud = 5;
+/*
+
+  CONTROLES:
+  
+  - click al centro para reiniciar con valores aleatorios
+  - tecla "p" para pausar / reanudar
+  - tecla "k" para reiniciar con valores originales
+  - tecla "s" para reiniciar con valores actuales
+  - tecla "w" para volver a velocidad por defecto
+  - tecla "a" para disminuir velocidad
+  - tecla "d" para aumentar velocidad
+
+*/
+
+
+let vel = 12;
+
+let anguloPar = 0.24;
+let anguloImpar = -0.47;
+let long = 1.9;
 
 let num;
-
-let sec_A, angulo_A, pasos_A;
-let sec_B, angulo_B, pasos_B;
+let sec_A, sec_B;
+let angulo_A, angulo_B;
 
 let p_x, p_y;
 
-let saltar, ciclo_A, ciclo_B;
+let inicio, saltar, ciclo_A, ciclo_B;
+
+let ent_x, ent_y;
+let aleatorio = false;;
 
 function setup() {
   createCanvas(512, 512);
   
-  num = 4;
-
+  num = 3;
+  inicio = true;
   saltar = false;
-  sec_A = num;
-  sec_B = num;
-  angulo_A = pasos_A = 0;
-  angulo_B = pasos_B = 0;
-  p_x = width / 2;
-  p_y = height * 0.9;
-
+  
   background(150);
-  translate(p_x, p_y);
-  noStroke();
-  fill(220, 199, 60);
-  circle(0, 0, 40);
-
-  //frameRate(1);
 }
 
 function draw() {
-  push();
-  translate(470, 370);
-  scale(0.1);
-  strokeWeight(11);
+  // VEL _________________________________________________
+  for (let i = 0; i < 100 * vel; i++) {
 
+    // evaluadores estado
+    if (Number.MAX_VALUE < 2 + 3 * sec_A) saltar = true;
+    else saltar = false;
+    
+    if (sec_A > 1) ciclo_A = true;
+    else ciclo_A = false;
 
-  console.log("{num: " + num +
-  " }  {sec_A: " + pasos_A + " " + sec_A +
-  " }  {sec_B: " + pasos_B + " " + sec_B + " }");
-
-  // evaluadores estado
-  if (3 * sec_A + 1 < Number.MAX_VALUE) saltar = false;
-  else saltar = true;
-
-  if (sec_A > 1) ciclo_A = true;
-  else ciclo_A = false;
-
-  if (sec_B > 1) ciclo_B = true;
-  else ciclo_B = false;
-  
-  // collatz A
-  if (ciclo_A && !saltar) {
-
-    if (sec_A % 2 == 0) {
-      sec_A = sec_A / 2;
-      angulo_A += anguloPar;
-    }
-    else {
-      sec_A = 3 * sec_A + 1;
-      angulo_A += anguloImpar;
-    }
-
-    pasos_A++;
-    translate(p_x, p_y);
-    rotate(angulo_A);
-    stroke(255);
-    line(0, 0, 0, - longitud);
-    p_x -= longitud * pasos_A * cos(angulo_A);
-    p_y -= longitud * pasos_A * sin(angulo_A);
-  }
-
-  // collatz B
-  else if (ciclo_B && !saltar) {
-
-    if (sec_B % 2 == 0) {
-      sec_B = sec_B / 2;
-      angulo_B += (anguloPar + Math.PI);
-    }
-    else {
-      sec_B = 3 * sec_B + 1;
-      angulo_B += (anguloImpar + Math.PI);
-    }
-
-    pasos_B++;
-    translate(p_x, p_y);
-    rotate(angulo_B);
-    stroke(0);
-    line(0, 0, 0, longitud);
-    p_x += longitud * pasos_A * cos(angulo_B); 
-    p_y += longitud * pasos_A * sin(angulo_B);
-  }
-
-  // nuevo numero
-  else {
-      num++;
+    if (sec_B > 1) ciclo_B = true;
+    else ciclo_B = false;
+    
+    // nuevo numero
+    if (inicio) {
       sec_A = num;
       sec_B = num;
-      angulo_A = pasos_A = 0;
-      angulo_B = pasos_B = 0;
+      angulo_A = angulo_B = 0;
+      pasos_A = pasos_B = 0;
       p_x = width / 2;
-      p_y = height * 0.9;
+      p_y = p_x;//height * 0.9;
+      inicio = false;
     }
 
+    // COLLATZ A
+    else if (ciclo_A && !saltar) {
+
+      // caso par
+      if (sec_A % 2 == 0) {
+        sec_A = sec_A / 2;
+        angulo_A += anguloPar;
+      }
+      // caso impar
+      else {
+        sec_A = 3 * sec_A + 1;
+        angulo_A += anguloImpar;
+      }
+      
+      resetMatrix();
+      translate(p_x, p_y);
+      stroke(255, 40);
+      line(0, 0, cos(angulo_A) * long, -sin(angulo_A) * long);
+      p_x += cos(angulo_A) * long;
+      p_y -= sin(angulo_A) * long;
+    }
+    
+    // COLLATZ B
+    else if (ciclo_B && !saltar) {
+
+      // caso par
+      if (sec_B % 2 == 0) {
+        sec_B = sec_B / 2;
+        angulo_B -= anguloPar;
+      }
+      // caso impar
+      else {
+        sec_B = 3 * sec_B + 1;
+        angulo_B -= anguloImpar;
+      }
+      
+      resetMatrix();
+      translate(p_x, p_y);
+      stroke(0, 50);
+      line(0, 0, -cos(angulo_B) * long, -sin(angulo_B) * long);
+      p_x -= cos(angulo_B) * long;
+      p_y -= sin(angulo_B) * long;
+    }
+
+    // fin numero
+    else {
+      num++;
+      saltar = false;
+      inicio = true;
+    }
+  }
+
+  if (dist(mouseX, mouseY, 256, 256) < 30) cursor(HAND);
+  else cursor(CROSS);
+}
+
+function mousePressed() {
+  push();
+  resetMatrix();
+  if (dist(mouseX, mouseY, 256, 256) < 30) {
+    anguloPar = random(0.2, 0.5);
+    anguloImpar = random(-0.4, 0.3);
+    long = random(1.5, 3);
+    setup();
+  }
   pop();
+}
+
+function keyPressed() {
+  if (key == 'w' || key == 'W') vel = 15;
+  if (key == 'd' || key == 'D') vel += vel / 2;
+  if (key == 'a' || key == 'A') {
+    if (vel <= 1) vel = 1; 
+    else vel -= vel / 2;
+  }
+  if (key == 's' || key == 'S') setup();
+  if (key == 'k' || key == 'K') {
+    vel = 15;
+    anguloPar = 0.24;
+    anguloImpar = -0.47;
+    long = 1.9;
+    setup();
+  }
+  if (key == 'p' || key == 'P') {
+    if (isLooping()) noLoop();
+    else loop();
+  }
 }
